c8fbe7751e023b90144acc696759e0c2bd4cac9d
Jesse Stewart <dj.lnxss@gmail.com>
2020-01-28 16:51:30 -0800

    Updated so that this api plugin is hosted on PyPI.

--- setup.py
+++ setup.py
@@ -1,14 +1,22 @@
-from setuptools import setup
+import setuptools
 
-setup(
-    name="sssapi",
-    packages=['sssapi'],
-    version='0.0.5',
-    author="snowshoestamp",
-    license="MIT",
-    author_email="hello@snowshoestamp.com",
-    url="https://github.com/snowshoestamp/python_sdk",
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sssapi-pkg", # Replace with your own username
+    version="0.1.0",
+    author="Snowshoe Stamps",
+    author_email="engineering@snowshoestamp.com",
     description="SnowShoeStamp Client Api Library",
-    zip_safe=False,
-    include_package_data=True
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/snowshoestamp/python_sdk",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=2.7',
 )
